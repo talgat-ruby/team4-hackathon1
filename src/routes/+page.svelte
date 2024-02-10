0b3866d03147bba745e3141c0f4469d60013edd0
nuliyev <jokerprodigy@gmail.com>
2024-02-10 16:07:17 +0500

    updated page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,30 @@
 <script lang="ts">
-
-import App from "$lib/App.svelte";
-import FormSteps from "$lib/FormSteps.svelte";
-
+	import App from '$lib/App.svelte';
+	import FormSteps from '$lib/FormSteps.svelte';
 </script>
 
 <body>
-<main>
-    <FormSteps/>
-    <App/>         
-</main>  
- </body>
-        <style>
-        body{
-        background: rgb(242, 242, 242);
-        }
-        main{
-        display: flex;
-        justify-content: center;
-        width: 1050px;
-        height: 600px;
-        margin-left: 251px;
-        margin-top: 105px;
-        border-radius: 15px;
-        
-        box-shadow: 0px 25px 40px -20px rgba(0, 0, 0, 0.1);
-        
-        background: rgb(255, 255, 255);
-        }
-        </style>
+	<main>
+		<FormSteps />
+		<App />
+	</main>
+</body>
+
+<style>
+	body {
+		background: rgb(242, 242, 242);
+	}
+	main {
+		display: flex;
+		justify-content: center;
+		width: 1050px;
+		height: 600px;
+		margin-left: 251px;
+		margin-top: 105px;
+		border-radius: 15px;
+
+		box-shadow: 0px 25px 40px -20px rgba(0, 0, 0, 0.1);
+
+		background: rgb(255, 255, 255);
+	}
+</style>
